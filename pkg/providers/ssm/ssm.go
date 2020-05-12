a611e82bc24b845fa97953bfdc728749bc06083b
TJ Leonard <terrance.j.leonard@gmail.com>
2020-05-11 20:37:41 -0400

    add ssm parameter versioning (#26)

--- pkg/providers/ssm/ssm.go
+++ pkg/providers/ssm/ssm.go
@@ -6,6 +6,7 @@ import (
 	"github.com/variantdev/vals/pkg/api"
 	"github.com/variantdev/vals/pkg/awsclicompat"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -17,12 +18,14 @@ type provider struct {
 	ssmClient *ssm.SSM
 
 	// AWS SSM Parameter store global configuration
-	Region string
+	Region  string
+	Version string
 }
 
 func New(cfg api.StaticConfig) *provider {
 	p := &provider{}
 	p.Region = cfg.String("region")
+	p.Version = cfg.String("version")
 	return p
 }
 
@@ -30,6 +33,9 @@ func New(cfg api.StaticConfig) *provider {
 func (p *provider) GetString(key string) (string, error) {
 	if key != "" && key[0] != '/' {
 		key = "/" + key
+	}
+	if p.Version != "" {
+		return p.GetStringVersion(key)
 	}
 
 	ssmClient := p.getSSMClient()
@@ -53,6 +59,48 @@ func (p *provider) GetString(key string) (string, error) {
 	p.debugf("SSM: successfully retrieved key=%s", key)
 
 	return *out.Parameter.Value, nil
+}
+
+func (p *provider) GetStringVersion(key string) (string, error) {
+	if key != "" && key[0] != '/' {
+		key = "/" + key
+	}
+
+	ssmClient := p.getSSMClient()
+	version, err := strconv.ParseInt(p.Version, 10, 64)
+
+	if err != nil {
+		return "", errors.New("version can't be converted to Int")
+	}
+
+	getParameterHistoryInput := &ssm.GetParameterHistoryInput{
+		Name:           aws.String(key),
+		WithDecryption: aws.Bool(true),
+	}
+
+	var result string
+	if err := ssmClient.GetParameterHistoryPages(getParameterHistoryInput, func(o *ssm.GetParameterHistoryOutput, lastPage bool) bool {
+		for _, history := range o.Parameters {
+			thisVersion := int64(0)
+
+			if history.Version != nil {
+				thisVersion = *history.Version
+			}
+			if thisVersion == version {
+				result = *history.Value
+				return false
+			}
+		}
+		return true
+	}); err != nil {
+		return "", errors.New(err.Error())
+	}
+	if result != "" {
+		p.debugf("SSM: successfully retrieved key=%s", result)
+		return result, nil
+	}
+
+	return "", errors.New("datasource.ssm.Get() out.Parameter.Value is nil")
 }
 
 func (p *provider) GetStringMap(key string) (map[string]interface{}, error) {
