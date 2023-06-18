35e9997152b8e94c12e9c37ee9b6489d9e8a76a7
Jan-Otto Kröpke <mail@jkroepke.de>
2023-06-18 20:04:02 +0200

    Add -decode-kubernetes-secrets flag to vals eval
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- cmd/vals/main_test.go
+++ cmd/vals/main_test.go
@@ -40,3 +40,37 @@ kind: Secret
 		t.Errorf("unexpected out: expected=%s, got=%s", outExpected, outActual)
 	}
 }
+
+func TestKsEncode(t *testing.T) {
+	in := `stringData:
+  foo: FOO
+kind: Secret
+`
+	outExpected := `data:
+  foo: Rk9P
+kind: Secret
+`
+	var inNode yaml.Node
+	if err := yaml.Unmarshal([]byte(in), &inNode); err != nil {
+		t.Fatalf("unmarshal: %v", err)
+	}
+
+	outNode, err := KsEncode(inNode)
+	if err != nil {
+		t.Fatalf("ksdecode: %v", err)
+	}
+
+	buf := &bytes.Buffer{}
+	encoder := yaml.NewEncoder(buf)
+	encoder.SetIndent(2)
+
+	if err := encoder.Encode(outNode); err != nil {
+		t.Fatalf("marshal: %v", err)
+	}
+
+	outActual := buf.String()
+
+	if outActual != outExpected {
+		t.Errorf("unexpected out: expected=%s, got=%s", outExpected, outActual)
+	}
+}
