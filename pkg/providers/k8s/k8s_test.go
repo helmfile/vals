003edf4f1c925fff563183349358ad8be758d661
Bernardo Salazar <bernardo.salazar@weareadaptive.com>
2023-12-14 18:38:49 +0100

    feat(k8s): more test coverage
    Signed-off-by: Bernardo Salazar <bernardo.salazar@weareadaptive.com>

--- pkg/providers/k8s/k8s_test.go
+++ pkg/providers/k8s/k8s_test.go
@@ -29,6 +29,7 @@ func Test_getObject(t *testing.T) {
 		want           map[string][]uint8
 		wantErr        string
 	}{
+		// valid kubeConfigPath is specified
 		{
 			namespace:      "test-namespace",
 			name:           "mysecret",
@@ -86,10 +87,12 @@ func Test_getObject(t *testing.T) {
 func Test_getKubeConfig(t *testing.T) {
 	homeDir, _ := os.UserHomeDir()
 	testcases := []struct {
-		config  config.MapConfig
-		want    string
-		wantErr string
+		config           config.MapConfig
+		kubeConfigEnvVar string
+		want             string
+		wantErr          string
 	}{
+		// kubeConfigPath is set
 		{
 			config: config.MapConfig{
 				M: map[string]interface{}{
@@ -107,11 +110,42 @@ func Test_getKubeConfig(t *testing.T) {
 			want:    "",
 			wantErr: "kubeConfigPath URI parameter is set but path /tmp/does-not-exist does not exist.",
 		},
+		// KUBECONFIG specified path is set
+		{
+			config: config.MapConfig{
+				M: map[string]interface{}{},
+			},
+			kubeConfigEnvVar: fmt.Sprintf("%s/.kube/config", homeDir),
+			want:             fmt.Sprintf("%s/.kube/config", homeDir),
+			wantErr:          "",
+		},
+		// KUBECONFIG specified path does not exist
+		{
+			config: config.MapConfig{
+				M: map[string]interface{}{},
+			},
+			kubeConfigEnvVar: "/tmp/does-not-exist",
+			want:             "",
+			wantErr:          "KUBECONFIG environment variable is set but path /tmp/does-not-exist does not exist.",
+		},
+		// defaultPath exists
+		{
+			config: config.MapConfig{
+				M: map[string]interface{}{},
+			},
+			kubeConfigEnvVar: "",
+			want:             fmt.Sprintf("%s/.kube/config", homeDir),
+			wantErr:          "",
+		},
 	}
 
 	for i := range testcases {
 		tc := testcases[i]
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			os.Unsetenv("KUBECONFIG")
+			if tc.kubeConfigEnvVar != "" {
+				os.Setenv("KUBECONFIG", tc.kubeConfigEnvVar)
+			}
 			got, err := getKubeConfig(tc.config)
 			if err != nil {
 				if err.Error() != tc.wantErr {
@@ -129,11 +163,13 @@ func Test_getKubeConfig(t *testing.T) {
 		})
 	}
 }
+
 func Test_getKubeContext(t *testing.T) {
 	testcases := []struct {
 		config config.MapConfig
 		want   string
 	}{
+		// Valid kubeContext is specified
 		{
 			config: config.MapConfig{
 				M: map[string]interface{}{
@@ -169,15 +205,53 @@ func Test_GetString(t *testing.T) {
 		want    string
 		wantErr string
 	}{
+		// Valid path is specified
 		{
 			path:    "v1/Secret/test-namespace/mysecret/key",
 			want:    "p4ssw0rd",
 			wantErr: "",
 		},
+		// Invalid path is specified
+		{
+			path:    "v1/Secret/test-namespace/mysecret/key/more/path",
+			want:    "",
+			wantErr: "Invalid path v1/Secret/test-namespace/mysecret/key/more/path. Path must be in the format <apiVersion>/<kind>/<namespace>/<name>/<key>",
+		},
+		// Bad path is specified
+		{
+			path:    "bad/data/path",
+			want:    "",
+			wantErr: "Invalid path bad/data/path. Path must be in the format <apiVersion>/<kind>/<namespace>/<name>/<key>",
+		},
+		// Non-existent namespace is specified
+		{
+			path:    "v1/Secret/badnamespace/secret/key",
+			want:    "",
+			wantErr: "Unable to get Secret badnamespace/secret: Unable to get the object from Kubernetes: secrets \"secret\" not found",
+		},
+		// Non-existent secret is specified
+		{
+			path:    "v1/Secret/test-namespace/badsecret/key",
+			want:    "",
+			wantErr: "Unable to get Secret test-namespace/badsecret: Unable to get the object from Kubernetes: secrets \"badsecret\" not found",
+		},
+		// Non-existent key is requested
 		{
 			path:    "v1/Secret/test-namespace/mysecret/non-existent-key",
 			want:    "",
 			wantErr: "Key non-existent-key does not exist in test-namespace/mysecret",
+		},
+		// Invalid apiVersion specified
+		{
+			path:    "v2/Secret/test-namespace/mysecret/non-existent-key",
+			want:    "",
+			wantErr: "Invalid apiVersion v2. Only apiVersion v1 is supported at this time.",
+		},
+		// Invalid kind specified
+		{
+			path:    "v1/ConfigMap/test-namespace/mysecret/non-existent-key",
+			want:    "",
+			wantErr: "Invalid kind ConfigMap. Only kind Secret is supported at this time.",
 		},
 	}
 	for _, tc := range tests {
