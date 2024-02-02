03ead58ccb425a953b13fcbfcd1002eff9baeae6
Chris Weyl <cweyl@ibm.com>
2024-02-02 14:01:16 -0600

    Load all common k8s auth providers
    Attempting to use vals (via helmfile) today, I ran into an error:
    
        ...Unable to get Secret ci-runners/runner-cos-cache-keys: Unable to
        create the Kubernetes client: no Auth Provider found for name "oidc"
    
    A quick search showed that the solution is to ensure the auth plugins
    are imported; testing this change locally with a rebuilt helmfile made
    my auth problems go away.
    
    Signed-off-by: Chris Weyl <cweyl@ibm.com>

--- pkg/providers/k8s/k8s.go
+++ pkg/providers/k8s/k8s.go
@@ -8,6 +8,7 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
